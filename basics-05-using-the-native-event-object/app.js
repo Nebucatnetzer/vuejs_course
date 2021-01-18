bc69656d06f33e02aa8738f07ed78e33570e82da
Andreas Zweili <andreas@2li.ch>
2021-01-18 21:34:43 +0100

    add a real example for watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -7,22 +7,16 @@ const app = Vue.createApp({
             // fullName: "",
         };
     },
-    // watch: {
-    //     name(value) {
-    //         if (value === "") {
-    //             this.fullName = "";
-    //         } else {
-    //             this.fullName = value + " " + this.lastName;
-    //         }
-    //     },
-    //     lastName(value) {
-    //         if (value === "") {
-    //             this.fullName = "";
-    //         } else {
-    //             this.fullName = this.name + " " + value;
-    //         }
-    //     },
-    // },
+    watch: {
+        counter(value) {
+            if (value > 50) {
+                const that = this;
+                setTimeout(function () {
+                    that.counter = 0;
+                }, 2000);
+            }
+        },
+    },
     computed: {
         fullName() {
             if (this.name === "" || this.lastName === "") {
