0ba28e968e919440d7f28613aafe5ce8ea178798
Andreas Zweili <andreas@2li.ch>
2021-01-18 17:52:39 +0100

    move the logic out of the HTML code

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
             counter: 0,
         };
     },
+    methods: {
+        increaseCounter() {
+            this.counter++;
+        },
+        decreaseCounter() {
+            this.counter--;
+        },
+    },
 });
 
 app.mount("#events");
