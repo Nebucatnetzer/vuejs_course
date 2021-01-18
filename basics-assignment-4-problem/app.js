a0dc7b7e6dae3ceb83702877a241935b47fcf1a0
Andreas Zweili <andreas@2li.ch>
2021-01-18 23:34:24 +0100

    using v-model is eaiser/faster

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
         return {
             user1: false,
             user2: false,
+            inputClass: "",
             paragraphVisible: true,
             paragraphHidden: false,
             inputColor: "",
@@ -21,24 +22,9 @@ const app = Vue.createApp({
         },
     },
     methods: {
-        getUserInput(event) {
-            const inputValue = event.target.value;
-            if (inputValue === "user1") {
-                this.user1 = true;
-            } else if (inputValue === "user2") {
-                this.user2 = true;
-            } else {
-                this.user1 = false;
-                this.user2 = false;
-            }
-        },
         toggleParagraphVisibility() {
             this.paragraphVisible = !this.paragraphVisible;
             this.paragraphHidden = !this.paragraphHidden;
-        },
-        setBackgroundColor(event) {
-            console.log(event.target.value);
-            this.inputColor = event.target.value;
         },
     },
 });
