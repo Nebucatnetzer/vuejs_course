725a1ed549f9485406f00d218aee4051d7be42c8
Andreas Zweili <andreas@2li.ch>
2021-01-25 17:49:25 +0100

    add an example App.vue file

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>My Friends</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "0123 4567 890",
+                    email: "manuel@localhost.com",
+                },
+                {
+                    id: "julie",
+                    name: "Julie Jones",
+                    phone: "9876 5432 210",
+                    email: "julie@localhost.com",
+                },
+            ],
+        };
+    },
+};
+</script>
