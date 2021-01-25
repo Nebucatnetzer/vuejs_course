ce366e1f58fbf297e2db3e1e931ef12ffdede56f
Andreas Zweili <andreas@2li.ch>
2021-01-25 12:33:19 +0100

    move the contact template to the component

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -23,9 +23,25 @@ const app = Vue.createApp({
 
 // use multiword component names
 app.component("user-contact", {
+    template: `
+            <li>
+                <h2>{{ friend.name }}</h2>
+                <button @click="toggleDetails()">Show Details</button>
+                <ul v-if="detailsAreVisible">
+                    <li><strong>Phone:</strong> {{ friend.phone }}</li>
+                    <li><strong>Email:</strong> {{ friend.email }}</li>
+                </ul>
+            </li>
+    `,
     data() {
         return {
             detailsAreVisible: false,
+            friend: {
+                id: "manuel",
+                name: "Manuel Lorenz",
+                phone: "01234 5678 991",
+                email: "manuel@localhost.com",
+            },
         };
     },
     methods: {
