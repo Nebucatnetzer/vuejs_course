2756418cec9f323404270ad557c71705d5995a17
Andreas Zweili <andreas@2li.ch>
2021-01-25 12:13:12 +0100

    setup basic app with data

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const app = Vue.createApp({
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "01234 5678 991",
+                    email: "manuel@localhost.com",
+                },
+                {
+                    id: "julie",
+                    name: "Julie Jones",
+                    phone: "09876 543 221",
+                    email: "julie@localhost.com",
+                },
+            ],
+        };
+    },
+    methods: {},
+});
+
+app.mount("app");
