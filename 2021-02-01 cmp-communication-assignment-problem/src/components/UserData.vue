ed8f6ea4582a5b211a8a0b0bafab3aee7cab1729
Andreas Zweili <andreas@2li.ch>
2021-02-02 22:19:36 +0100

    emit the form data in UserData

--- 2021-02-01 cmp-communication-assignment-problem/src/components/UserData.vue
+++ 2021-02-01 cmp-communication-assignment-problem/src/components/UserData.vue
@@ -3,11 +3,11 @@
         <form @submit.prevent="returnData">
             <div>
                 <label>Username: </label>
-                <input type="text" />
+                <input type="text" v-model="enteredUsername" />
             </div>
             <div>
                 <label>Age: </label>
-                <input type="number" />
+                <input type="number" v-model="enteredAge" />
             </div>
             <div><button>Add User</button></div>
         </form>
@@ -16,8 +16,21 @@
 
 <script>
 export default {
+    emits: ["active-user"],
+    data() {
+        return {
+            enteredUsername: "",
+            enteredAge: "",
+        };
+    },
     methods: {
-        returnData() {},
+        returnData() {
+            const user = {
+                username: this.enteredUsername,
+                age: Number(this.enteredAge),
+            };
+            this.$emit("active-user", user);
+        },
     },
 };
 </script>
