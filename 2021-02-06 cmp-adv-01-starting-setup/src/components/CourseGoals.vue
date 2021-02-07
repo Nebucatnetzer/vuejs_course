6e3c4e4a254dce784d07e5ed05bdc8d284c19446
Andreas Zweili <andreas@2li.ch>
2021-02-07 11:17:20 +0100

    lesson 113: add a scoped slot

---
+++ 2021-02-06 cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul v-for="goal in goals" :key="goal">
+            <slot :item="goal"></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ["Finish the course", "Learn Vue"],
+        };
+    },
+};
+</script>
+
+<style></style>
