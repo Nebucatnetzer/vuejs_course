f3533827a239c5935c319d9e0cee13754eec8e3f
Andreas Zweili <andreas@2li.ch>
2021-02-08 12:00:06 +0100

    lesson 124: add a resource list

---
+++ 2021-02-08 prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource.title }}
+      <ul>
+        <li>
+          {{ resource.description }}
+        </li>
+        <li>
+          <a :href="resource.link">{{ resource.link }}</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Oficial Guide',
+          description: 'The official documentation for Vue.js.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'duckduckgo',
+          title: 'Duckduckgo',
+          description: 'A search engine that values privacy.',
+          link: 'https://duckduckgo.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
