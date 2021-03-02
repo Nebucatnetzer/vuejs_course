a6760b1ba403814e9a14bf5618802f2cc88437e7
Andreas Zweili <andreas@2li.ch>
2021-03-02 22:21:36 +0100

    clear the console after a reload

--- 2021-02-08 2024 forms-01-starting-setup/src/main.js
+++ 2021-02-08 2024 forms-01-starting-setup/src/main.js
@@ -3,3 +3,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+if (module.hot) {
+  module.hot.accept(); // already had this init code
+
+  module.hot.addStatusHandler(status => {
+    if (status === 'prepare') console.clear();
+  });
+}
