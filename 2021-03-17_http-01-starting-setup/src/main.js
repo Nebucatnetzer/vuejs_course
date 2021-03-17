4350cd5bb536a4f0efb8aae6ec30247dd4b7a621
Andreas Zweili <andreas@2li.ch>
2021-03-17 20:54:05 +0100

    add a new project

---
+++ 2021-03-17_http-01-starting-setup/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+
+app.mount('#app');
+
+if (module.hot) {
+  module.hot.accept(); // already had this init code
+
+  module.hot.addStatusHandler(status => {
+    if (status === 'prepare') console.clear();
+  });
+}
