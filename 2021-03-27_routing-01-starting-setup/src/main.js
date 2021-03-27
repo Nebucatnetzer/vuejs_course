c82c5c94f6508473338453c8660d1a1b0eb0e114
Andreas Zweili <andreas@2li.ch>
2021-03-27 20:36:12 +0100

    add code for toastlog

--- 2021-03-27_routing-01-starting-setup/src/main.js
+++ 2021-03-27_routing-01-starting-setup/src/main.js
@@ -2,6 +2,14 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.mount('#app');
+
+if (module.hot) {
+  module.hot.accept(); // already had this init code
+
+  module.hot.addStatusHandler(status => {
+    if (status === 'prepare') console.clear();
+  });
+}
