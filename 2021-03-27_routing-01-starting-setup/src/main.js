68f4aa7168984b65dbeed168f57674c491f9c10f
Andreas Zweili <andreas@zweili.ch>
2021-04-26 10:09:17 +0200

    175 add a nested route

--- 2021-03-27_routing-01-starting-setup/src/main.js
+++ 2021-03-27_routing-01-starting-setup/src/main.js
@@ -12,9 +12,14 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: TeamsList },
-    { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true }
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { path: ':teamId', component: TeamMembers, props: true }
+      ]
+    },
+    { path: '/users', component: UsersList }
   ]
 });
 
