9f67d9339efeb72862a514e99aeff64cd3fcef11
Andreas Zweili <andreas@zweili.ch>
2021-05-05 14:17:09 +0200

    setup the store

---
+++ 2021-05-05_vuex-11-a-challenge-starting-code/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+import cartModule from './modules/cart/index.js';
+import productsModule from './modules/products/index.js';
+
+const store = createStore({
+  modules: {
+    cart: cartModule,
+    products: productsModule
+  },
+  state() {
+    return {};
+  }
+});
+
+export default store;
---
+++ 2021-05-05_vuex-11-a-challenge-starting-code/src/store/modules/products/index.js
@@ -0,0 +1,11 @@
+//import counterActions from './actions.js';
+//import counterGetters from './getters.js';
+//import counterMutations from './mutations.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+    };
+  },
+};
